2b9c9272a214a3f6467e160d0400f39e50da4641
sakshichordia <sakshichordia1307@gmail.com>
2025-09-13 20:41:18 -0400

    Initial Spring Boot project

---
+++ src/main/java/com/sakshi/sra/smartresearchassistant/SmartResearchAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.sakshi.sra.smartresearchassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartResearchAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartResearchAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sakshi/sra/smartresearchassistant/SmartResearchAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakshi.sra.smartresearchassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartResearchAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
